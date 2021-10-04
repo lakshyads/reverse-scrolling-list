3322accd2ab9fe8097bdaccf535a830657a4ade7
Lakshya Dev <lakshyadev@live.com>
2021-10-04 15:28:14 +0530

    Updated app to use components, added hooks and handlers

--- src/App.js
+++ src/App.js
@@ -3,95 +3,107 @@ import { useState, useRef, useEffect } from 'react';
 import useOnScreen from './hooks/useOnScreen';
 import { fetchDataByPage } from './utils/pagination.utils';
 
+import Header from './components/Header';
+import Input from './components/Input';
+import Button from './components/Button';
+import DisplayList from './components/DisplayList';
+import DisplayListItem from './components/DisplayListItem';
+
 export default function App() {
   // =======================================================================
-  // Component states
+  // Display list state & handlers
   // -----------------------------------------------------------------------
-  // Pagination
-  const pageSize = 20;
-  const [currentPage, setCurrentPage] = useState(1);
-  // Input & List
-  const [inp, setInp] = useState(null);
-  const [listData, setListdata] = useState([]);
-  // List to keep track of all expanded records
-  const [currentExpanded, setCurrentExpanded] = useState([]);
-  // To keep track of newly added record (will be used to scroll to end of the list)
-  const [totalNewRecords, setTotalNewRecords] = useState(0);
-
-  // Scrollable ref - To end of list when new record is added
-  const listEndRef = useRef();
-  useEffect(() => listEndRef.current.scrollIntoView(), [totalNewRecords]);
+  const [listItems, setListItems] = useState([]);
+  const [fetchedDataItems, setFetchedDataItems] = useState([]);
+  const scrollToEndRef = useRef();
+  const scrollToLoadedEndRef = useRef();
 
   // =======================================================================
-  // Infinite scroll
+  // Form state & handlers
   // -----------------------------------------------------------------------
-  const loadDataRef = useRef();
-  const shouldLoadMoreData = useOnScreen(loadDataRef);
-
-  useEffect(() => {
-    if (shouldLoadMoreData) setCurrentPage(currentPage + 1);
-  }, [shouldLoadMoreData, currentPage]);
-
-  useEffect(() => {
-    let loadedData = [];
-    try {
-      loadedData = fetchDataByPage(currentPage, pageSize);
-    } catch (error) {
-      alert(error);
-    }
-    setListdata(loadedData.concat(listData.slice(0 - totalNewRecords)));
-  }, [currentPage]);
-  // ========================================================================
-
+  const [inputValue, setInputValue] = useState('');
+  const resetInputValue = () => setInputValue('');
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    if (value === null || value === undefined) resetInputValue();
+    else setInputValue(value);
+  };
   /**
-   * Handle add new record to list action
+   * Handler for add item to list button
    */
   const handleAddToList = () => {
-    if (inp === null || inp === undefined) return;
-
-    setListdata(listData.concat(inp));
-    toggleExpand(listData.length);
-    setInp(null);
-    setTotalNewRecords(totalNewRecords + 1);
+    if (inputValue === '') return;
+    setListItems(
+      listItems.concat(
+        <DisplayListItem value={inputValue} expandByDefault={true} />
+      )
+    );
+    resetInputValue();
+    // Once the item is added to list, scrill to it
+    scrollToEndRef.current.scrollIntoView({
+      behaviour: 'smooth',
+    });
   };
 
-  /**
-   * Toggle to expand/collapse a record
-   * @param {*} index Index of record to expand
-   */
-  const toggleExpand = (index) => {
-    if (currentExpanded.includes(index))
-      setCurrentExpanded(currentExpanded.filter((ind) => ind !== index));
-    else setCurrentExpanded(currentExpanded.concat(index));
-  };
+  // =======================================================================
+  // Infinite scroll+pagination state & handlers
+  // -----------------------------------------------------------------------
+  const pageSize = 20; // Items per page
+  const [currentPage, setCurrentPage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
+  const requestPrevPage = () => {
+    setIsLoading(true);
+    setCurrentPage(currentPage + 1);
+  };
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const dataItems = await fetchDataByPage(currentPage, pageSize);
+        if (fetchedDataItems.length > 0)
+          setListItems(fetchedDataItems.concat(listItems));
+        setFetchedDataItems(
+          dataItems.map((item) => <DisplayListItem value={item} key={item} />)
+        );
+      } catch (error) {
+        alert(error);
+      } finally {
+        scrollToLoadedEndRef.current.scrollIntoView({ behaviour: 'smooth' });
+        setIsLoading(false);
+      }
+    };
+    if (currentPage !== null) fetch();
+  }, [currentPage]);
+
+  // =======================================================================
+  // Render
+  // -----------------------------------------------------------------------
   return (
     <div className='App'>
-      <h1>Wipro Telstra</h1>
-      <h2>React Assignment 1</h2>
-      <input
-        placeholder='type here'
-        value={inp ? inp : ''}
-        onChange={(e) => setInp(e.target.value)}
+      {/* Header */}
+      <Header assignmentNo={1} />
+      <br />
+
+      {/* Controlled Form */}
+      <Input value={inputValue} handleOnChange={handleInputChange} />
+      <Button text={'X'} onClick={resetInputValue} />
+      <Button
+        className={'submit-button'}
+        text={'Add to list'}
+        onClick={handleAddToList}
       />
-      &nbsp;
-      <button onClick={handleAddToList}>Add to list</button>
       <br />
       <br />
-      <div className='display-box' style={{ maxWidth: '500px' }}>
-        <div ref={loadDataRef}>Load more</div>
-        {listData.map((val, index) => (
-          <div key={index}>
-            <div onClick={() => toggleExpand(index)}>
-              {val} (click me to expand)
-            </div>
-            {currentExpanded.includes(index) && (
-              <div className='expanded'>{val} expanded</div>
-            )}
-          </div>
-        ))}
-        <div ref={listEndRef} />
-      </div>
+      {/* Display list */}
+      <DisplayList
+        listItems={listItems}
+        fetchedItems={fetchedDataItems}
+        isLoading={isLoading}
+        scrollToEndRef={scrollToEndRef}
+        scrollToLoadedEndRef={scrollToLoadedEndRef}
+        requestPrevPage={requestPrevPage}
+        setIsLoading={setIsLoading}
+      />
     </div>
   );
 }
